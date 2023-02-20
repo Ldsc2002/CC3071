20cee7a2ad4fd2ef2ff7d909c7d0efbbcd97dae5
Ldsc2002 <luissantoscuellar@gmail.com>
2023-02-20 13:02:38 -0600

    Fixed issues with epsilon

--- modules/common/node.py
+++ modules/common/node.py
@@ -1,19 +1,28 @@
 import pydot
+from modules.common.symbol import *
 
-class Node():
+class Node(object):
     def __init__(this, value, left = None, right = None):
-        this.value = value
+        this.value = Symbol(value)     
         this.left = left
         this.right = right
     
     def printTree(this, graph):
-        thisNode = pydot.Node(this.value)
+        thisNode = pydot.Node(this.character)
         graph.add_node(thisNode)
 
         if this.left is not None:
             this.left.printTree(graph)
-            graph.add_edge(pydot.Edge(thisNode, this.left.value))
+            graph.add_edge(pydot.Edge(thisNode, this.left.character))
 
         if this.right is not None:
             this.right.printTree(graph)
-            graph.add_edge(pydot.Edge(thisNode, this.right.value))
+            graph.add_edge(pydot.Edge(thisNode, this.right.character))
+
+    def __getattribute__(this, name: str):
+        if name == 'value':
+            return object.__getattribute__(this, name).id
+        elif name == 'character':
+            return object.__getattribute__(this, "value").cid
+        else:
+            return object.__getattribute__(this, name)
--- modules/common/symbol.py
+++ modules/common/symbol.py
@@ -2,7 +2,7 @@ class Symbol():
     def __init__(this, symbolID):
         this.id = symbolID
         
-        if symbolID == 'epsilon':
+        if symbolID == 'epsilon' or symbolID == 'E':
             this.cid = 'ε'
         else:
             this.cid = symbolID
