0e8e33e6671b57c7f02df21395e20eb65ba3a262
Ldsc2002 <luissantoscuellar@gmail.com>
2023-02-20 20:41:17 -0600

    Code cleanup

--- modules/driver.py
+++ modules/driver.py
@@ -15,20 +15,3 @@ def generateNFA(regex):
     tree.printTree()
 
     NFA(tree.tree).createImage()
-
-def testRun():
-    print('\n----- Testing Infix to AFN -----')
-    testString = 'a|b'
-    correctString = 'ab|'
-
-    print('\nTest 1: Infix to Postfix')
-    test = Regex(testString).postfix
-    print('Expected: ' + correctString + ' --- Actual: ' + test)
-    assert test == correctString
-
-    print('\nTest 2: Regex Tree')
-    test = RegexTree(test)
-    test.printTree()
-
-    print('\nTest 3: NFA')
-    NFA(test.tree).createImage()
