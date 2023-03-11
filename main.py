b9c18963a65842c927335f582c45fb63e36fd8b7
Ldsc2002 <luissantoscuellar@gmail.com>
2023-03-10 20:16:50 -0600

    Added menu

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from modules.driver import generateNFA, textInput, isDebugging
+from modules.driver import generateNFA, textInput, isDebugging, menuInput, selectOption
+options = [
+    "(a*|b*)c",
+    "(b|b)*abb(a|b)*",
+    "(a|E)b(a+)c?",
+    "(a|b)*a(a|b)(a|b)",
+    "b*ab?",
+    "b+abc+",
+    "0(0|1)*0",
+    "((E|0)1*)*",
+    "(0|1)*0(0|1)(0|1)",
+    "(00)*(11)*",
+    "0|1)1*(0|1)",
+    "0?(1|E)?0*",
+    "((1?)*)*",
+    "(01)*(10)*",
+    "a(a?b*|c+)b|baa"
+]
+
 
 if __name__ == '__main__':
     if isDebugging():
-        # regex = "a(a?b*|c+)b|baa"
-        regex = "((E|0)1*)*"
+        regex = "a(a?b*|c+)b|baa"
         simulate = "baa"
     
     else:
-        regex = textInput("Please enter a regular expression", 1)
+        operationMode = menuInput(["Select regex from defaults list", "Input regex manually"], "Select an operation mode")
+
+        if operationMode == 1:
+            regex = selectOption(options, "Select a regex")
+
+        elif operationMode == 2:
+            regex = textInput("Please enter a regular expression", 1)
+        
         simulate = textInput("Please enter a word to simulate (leave blank to skip)")
     
     if len(simulate) > 0:
