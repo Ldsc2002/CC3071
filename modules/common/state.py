7fd7e5b1015461650b85f4a45ecdbcca2e59604d
Ldsc2002 <luissantoscuellar@gmail.com>
2023-05-12 21:26:10 -0600

    WIP working initial state

--- modules/common/state.py
+++ modules/common/state.py
@@ -2,9 +2,10 @@ from modules.proto.type import *
 from modules.common.set import *
 
 class State():
-    def __init__(this, stateID = '', tokenID = ""):
+    def __init__(this, stateID = '', tokenID = "", type = 1):
         this.id = stateID
         this.tokenID = tokenID
+        this.type = Type(type)
 
     def __str__(this):
         return str(this.id)
--- modules/components/yaparParser.py
+++ modules/components/yaparParser.py
@@ -1,13 +1,14 @@
+from modules.common.set import Set
+from modules.common.state import State
+
 class YaparParser():
     def __init__(this, file, yalex):
         this.file = file
         this.yalex = yalex
         this.parse()
+        this.parseSLR()
 
     def parse(this):        
-        def closure(production):
-            newProduction = [production]
-
         tokens = []
         productions = {}
         readingTokens = True
@@ -49,6 +50,7 @@ class YaparParser():
                         line = line.strip()
                         
                         if prevProduction == None:
+                            line = line.replace(":", "")
                             productions[line] = []
                             prevProduction = line
 
@@ -81,6 +83,69 @@ class YaparParser():
             if token not in yalexTokens:
                 raise Exception("Token '" + token + "' not found in yalex file")
 
-        # TODO delete
-        print("Tokens: " + str(tokens))
-        print("Productions: " + str(productions))
+
+        this.tokens = tokens
+        this.productions = productions
+
+    def parseSLR(this):
+        def closure(production):
+            productions = [production]
+
+            for p1 in productions:
+                for p2 in grammar:
+                    term1 = p1.split('->')[1].split('.')[1].strip().split(' ')[0].strip()
+                    term2 = p2.split('->')[0].strip()
+
+                    if term1 == term2:
+                        p2 = p2.split('->')
+                        p2[1] = p2[1].strip().split('.')[1]
+                        p2[1] = ' . ' + p2[1].strip()
+                        p2 = '->'.join(p2)
+
+                        if p2 not in productions:
+                            productions.append(p2)
+        
+            return productions
+                            
+        def goto(production, symbol):            
+            pass
+
+        terminals = this.tokens
+        symbols = Set()
+        grammar = Set()
+        initialGrammar = ""
+
+        newItem = {list(this.productions.keys())[0] + "'": [this.productions[list(this.productions.keys())[0]][0]]}
+        this.productions = {**newItem, **this.productions}
+
+        for token, production in this.productions.items():
+            production.insert(0, '.')
+
+            tempProduction = ""
+            for p in production:
+                if p not in symbols:
+                    symbols.add(p)
+
+                if p == "|":
+                    tempProduction = tempProduction.strip()
+                    grammar.add(token + " -> " + tempProduction)
+                    tempProduction = ". "
+                    continue
+
+                tempProduction += p + " "
+                
+            tempProduction = tempProduction.strip()
+
+            if token not in terminals and initialGrammar == "":
+                initialGrammar = token + " -> " + tempProduction
+
+            grammar.add(token + " -> " + tempProduction)
+
+        this.initial = State(closure(initialGrammar), type=0) 
+        this.states = [this.initial]
+
+        for state in this.states:
+            for symbol in symbols:
+                nextState = goto(state, symbol)
+
+        print("DONE")
